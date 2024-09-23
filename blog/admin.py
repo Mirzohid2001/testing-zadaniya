1cace15c64a27474b57a54c8060ae92eb039d2f8
Mirzohid Kenjayev <boburovich0508@gmail.com>
2024-09-23 13:14:16 +0500

    admin reeady

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
+
 
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('title', 'description', 'owner__username')
